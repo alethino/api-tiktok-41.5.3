fd1272f290848a340682e44b10d6e261834e3452
neo galaxy <webqart@icloud.com>
2025-07-14 09:03:56 +0300

    Update api.js

--- api.js
+++ api.js
@@ -1,3 +1,7 @@
+### contact me
+**Contact for api :** [https://t.me/Neo_Galaxy](https://t.me/Neo_Galaxy)
+## telegram: @Neo_Galaxy
+
 "use strict";
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     return new (P || (P = Promise))(function (resolve, reject) {
